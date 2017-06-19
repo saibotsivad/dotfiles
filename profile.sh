42b9f815a23d3ceb20deb4da78e64913cf25bf21
Tobias Davis <accounts@davistobias.com>
2017-06-19 10:23:38 -0500

    copying over some stuff from local

--- profile.sh
+++ profile.sh
@@ -7,21 +7,26 @@ source $SCRIPTS/real-easy.sh
 
 export NPM_GLOBAL_PATH=$DEVELOPMENT/npm_global/bin
 export MONGODB_BIN_PATH=$DEVELOPMENT/mongodb/bin
-export GAME_SIMULATOR_PATH=$DEVELOPMENT/git/fanx-game-simulator
 
-export PATH=$NPM_GLOBAL_PATH:$MONGODB_BIN_PATH:$GAME_SIMULATOR_PATH:$PATH
+export PATH=$NPM_GLOBAL_PATH:$MONGODB_BIN_PATH:$PATH
 
-alias sublime="open -a 'Sublime Text.app'"
-alias showhidden="defaults write com.apple.finder AppleShowAllFiles YES; killall -KILL Finder"
-alias reload="source ~/.profile"
-alias dnsflush="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder; echo dns flushed"
+# useful commands
+alias sublime="open -a 'Sublime Text.app'" # open a file in sublime text editor
+alias showhidden="defaults write com.apple.finder AppleShowAllFiles YES; killall -KILL Finder" # show hidden files
+alias reload="source ~/.profile" # reload this file
+alias dnsflush="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder; echo dns flushed" # flush dns cache
 
+# installed apps
 alias redis="~/Development/redis/src/redis-server"
 alias redis-cli="~/Development/redis/src/redis-cli"
 
-alias nr="npm-run"
+# shortcuts
+alias qrcode=qrcode-terminal # create qrcode from given text
+alias nr=npm-run # run with npm scoped environment
+alias og=gh-home # open current folder in github if possible
+alias on=npm-home # open current folder in npm if possible
 
 export PS1="\n$C_LIGHTGREEN\u $C_LIGHTGRAY@ $C_LIGHTGREEN\h $C_LIGHTGRAY: $C_LIGHTYELLOW\w $C_LIGHTCYAN"'$(__git_ps1 " (%s)")'"\n$C_LIGHTGRAY\$ $C_DEFAULT "
 
-export NVM_DIR="/Users/cri-admin/.nvm"
+export NVM_DIR="~/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm

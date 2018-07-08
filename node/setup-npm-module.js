99ab621fff5641d0bef08940ae0c744f738d2cf4
Tobias Davis <accounts@davistobias.com>
2018-07-07 23:36:59 -0500

    feature: make the final step be a git commit

--- node/setup-npm-module.js
+++ node/setup-npm-module.js
@@ -20,19 +20,23 @@ const devDependencies = [
 	'tape@^4.6.3'
 ]
 
-
 module.exports = async ({ cwd, argv }) => {
 	// write files
 	const partsToReplace = {
 		'{{REPO_NAME}}': path.parse(cwd).base,
 		'{{DESCRIPTION}}': await waitForUserInput('Add a description (you can update later): '),
-		'{{KEYWORDS}}': JSON.stringify((await waitForUserInput('Type some keywords (space seperated): ')).split(/\s+/))
+		'{{KEYWORDS}}': JSON.stringify((await waitForUserInput('Type some keywords (space seperated): ')).split(/\s+/)),
+		'{{CURRENT_DATE}}': new Date().toISOString().split('T')[0]
 	}
 	files
 		.forEach(({ content, filename }) => {
-			const data = Object
+			let data = Object
 				.keys(partsToReplace)
 				.reduce((text, key) => text.replace(new RegExp(key, 'g'), partsToReplace[key]), content)
+			// a nice touch: reformat the package.json so future commits don't change it
+			if (filename === 'package.json') {
+				data = JSON.stringify(JSON.parse(data), undefined, 2)
+			}
 			fs.writeFileSync(path.join(cwd, filename), data, { encoding: 'utf8' })
 		})
 
@@ -42,4 +46,10 @@ module.exports = async ({ cwd, argv }) => {
 			console.log(`installing: ${dependency}`)
 			sh`npm install --save-dev ${dependency}`
 		})
+
+	// commit it as version 0
+	sh`git init`
+	sh`git add -A`
+	sh`git commit -m "0.0.0"`
+	sh`git tag -a v0.0.0 -m "chore: initialize project using automated script"`
 }

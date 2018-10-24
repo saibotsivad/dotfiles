284d2018e88f34dcfbb905eea406248dfb459806
Tobias Davis <tdavis@fnoex.com>
2018-10-24 09:27:36 -0500

    feature: add some aliases and a ruby version manager setup

--- profile-fno.sh
+++ profile-fno.sh
@@ -12,6 +12,12 @@ source $SCRIPTS/profile.sh
 
 # ========== do customization after here ==========
 
+# setup rbenv, a ruby environment manager
+# installation of `rbenv` is done with homebrew: `brew install rbenv`
+eval "$(rbenv init -)"
+
 # setup anything custom
 export GAME_SIMULATOR_PATH=$DEVELOPMENT/git/fanx-game-simulator
 export PATH=$GAME_SIMULATOR_PATH:$PATH
+alias chrome="open -a Google\ Chrome --args --disable-web-security --user-data-dir"
+alias startdb="mongod --config /usr/local/etc/mongod.conf"

27006b4db8ac1ab0e38ed7d4f015eb85375dc688
Tobias Davis <tdavis@fnoex.com>
2019-11-06 21:53:43 -0600

    some snippets from FNO laptop

--- profile-fno.sh
+++ profile-fno.sh
@@ -21,8 +21,31 @@ source $SCRIPTS/profile.sh
 # e.g. NOT `bundler` it's `bundle`
 eval "$(rbenv init -)"
 
+# make sure python installed programs are accessible
+# install pip using `easy_install pip`
+export PATH="/Users/tdavis/Library/Python/2.7/bin":$PATH
+
+# used for 'localstack' an AWS emulator
+# export SERVICES=apigateway,kinesis,dynamodb,dynamodbstreams,s3,lambda,sns,sqs,iam
+export SERVICES=dynamodb
+
 # setup anything custom
 export GAME_SIMULATOR_PATH=$DEVELOPMENT/git/fanx-game-simulator
 export PATH=$GAME_SIMULATOR_PATH:$PATH
 alias chrome="open -a Google\ Chrome --args --disable-web-security --user-data-dir"
 alias startdb="mongod --config /usr/local/etc/mongod.conf"
+alias noder="node -r 'hard-rejection/register'"
+alias faucet="node_modules/.bin/faucet"
+alias fanx="/Users/tdavis/Development/git/fanx-api-aws/bin/cli.js"
+
+alias ssh-stats-qa="ssh -i /Users/tdavis/.ssh/stats-ftp-qa-key.pem ubuntu@52.54.108.2"
+alias ssh-stats-prod="ssh -i /Users/tdavis/.ssh/stats-ftp-key.pem ubuntu@100.26.80.139"
+
+function udate () {
+    echo "process.stdout.write(('${1}' && new Date(parseInt('${1}', 10) * 1000) || new Date()).toISOString())" | node
+}
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*

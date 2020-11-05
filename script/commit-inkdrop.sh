a78db0094a55e6356cf28b897c76fc4117f0c314
Tobias Davis <accounts@davistobias.com>
2020-11-04 23:05:21 -0600

    the inkdrop sync script builds the blockdown binder

--- script/commit-inkdrop.sh
+++ script/commit-inkdrop.sh
@@ -3,7 +3,9 @@
 #
 # This command is used by a cronjob to commit any changes
 # that might have synced to Inkdrop's backup. It requires
-# the environment variable $INKDROP_BACKUP_FOLDER to be set.
+# the environment variable $INKDROP_BACKUP_FOLDER to be set,
+# and the `inkdrop-to-binder` npm library to be installed
+# globally.
 #
 # Add it to your Mac cronjob via:
 #
@@ -21,6 +23,10 @@ commit_inkdrop_changes () {
 	cd $INKDROP_BACKUP_FOLDER
 	if [ `git ls-files -m -o | wc -l` -ne 0 ]; then
 		echo "[$DATE] there are changes"
+		rm -rf $INKDROP_BACKUP_FOLDER/inkdrop-binder
+		inkdrop-to-binder \
+		  --input=$INKDROP_BACKUP_FOLDER/raw-inkdrop-data \
+		  --output=$INKDROP_BACKUP_FOLDER/inkdrop-binder
 		git add -A
 		git commit -am "cronjob update"
 		git push -u origin master

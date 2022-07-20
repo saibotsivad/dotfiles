4815f9ead41c71bed6c4ed92bf0d99891da4609b
Tobias Davis <tobias@davistobias.com>
2022-07-19 22:11:58 -0500

    feature: add cargo

--- profile-mbair.sh
+++ profile-mbair.sh
@@ -16,5 +16,3 @@ source $SCRIPTS/profile.sh
 # export BEGINPM_ROOT="$DEVELOPMENT/beginpm"
 # or
 # alias init="beginpm saibotsivad/beginpm-opinions"
-
-alias cordova="../node_modules/cordova/bin/cordova"
